e835815e86776d6d38ef292f7c0107c970edd854
shashanka2a <37143174+shashanka2a@users.noreply.github.com>
2025-10-18 05:24:32 -0700

    Change 'weds' text color to grey for softer appearance

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ export default function App() {
               color: '#6B3FA0'
             }}>
               <span>Deekshitha</span>
-              <span className="text-black font-semibold">weds</span>
+              <span className="text-gray-600 font-semibold">weds</span>
               <span>Harish</span>
             </h1>
           </motion.div>
